a5e9d0e1308f20469a5b742c85967690ad149dd3
Piairecarre <38073787+Piairecarre@users.noreply.github.com>
2018-04-05 15:42:09 +0200

    Update Anne_bissextile.rb

--- Anne_bissextile.rb
+++ Anne_bissextile.rb
@@ -17,16 +17,14 @@ end
 print "Pour connatre les 20 annees bissextiles qui suivent une annee donnee, saisissez cette annee : "
 An = Integer(gets.chomp)
 h_bissextile = Init_h_bissext (An)
-puts h_bissextile
 
 h_bissextile.each do |a_iter, val|
   h_bissextile[a_iter] = Bissextile? (a_iter)
 end
-puts h_bissextile
 
 puts "Voici les 20 annees bissextiles a venir :"
 h_bissextile.each do |a_test, val|
   if val == "Bissextile : 366 jours"
-    print " #{a_test}"
+    puts " #{a_test}"
   end
 end
